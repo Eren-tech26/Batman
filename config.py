077dcc7f161ccfe53773cf0fe5c5d4fec0b80196
Eren-tech26 <rohanishwarkatti03@gmail.com>
2025-09-22 22:41:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,9 +22,11 @@ BOT_NAME = "˹𝘽ᴀᴛᴍᴀɴ ꭙ 𝙈ᴜ𝙨ɪᴄ˼™⚡️"
 ASSUSERNAME = "@batmanplayzz"
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://thebiggestcomebackever:EREN1234@cluster0.7q7ri.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
 API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
 VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
-API_KEY = getenv("API_KEY", "30DXNexGenBots107029")
+API_KEY = getenv("API_KEY","30DXNexGenBots107029")
+
 DURATION_LIMIT_MIN = 500000
 
 LOGGER_ID = int(getenv("LOGGER_ID", "-1002812568647"))
